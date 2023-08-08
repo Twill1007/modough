c3f1e5c2c43798f8fab6dcca53d2272f60549a3e
Twill1007 <85330522+Twill1007@users.noreply.github.com>
2023-08-08 16:08:43 -0700

    CSS Update

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,12 +1,22 @@
 import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 import HeaderCartButton from "./HeaderCartButton";
 import classes from "./MainNavigation.module.css";
+import Login from "../../pages/Login";
+import Register from "../../pages/Register";
 
 const MainNavigation = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>Mo's Dough!</h1>
+
+        <Link to="/register">
+          <button className={classes.registerButton}>Register</button>
+        </Link>
+        <Link to="/login">
+          <button className={classes.loginButton}>Login</button>
+        </Link>
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
     </Fragment>
