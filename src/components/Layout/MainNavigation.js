a67f769c853dcbdf89888d8af489b3dddfea7a31
Twill1007 <85330522+Twill1007@users.noreply.github.com>
2023-08-18 15:29:47 -0700

    NavBar Update

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,21 +1,26 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { Link } from "react-router-dom";
 import HeaderCartButton from "./HeaderCartButton";
 import classes from "./MainNavigation.module.css";
 
 const MainNavigation = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>Mo's Dough!</h1>
-
-        <Link to="/auth">
-          <button className={classes.registerButton}>Register</button>
-        </Link>
+        {isLoggedIn && (
+          <Link to="/auth">
+            <button className={classes.registerButton}>Login/Sign Up</button>
+          </Link>
+        )}
         <Link to="/login">
           <button className={classes.loginButton}>Login</button>
         </Link>
-        <HeaderCartButton onClick={props.onShowCart} />
+        {!isLoggedIn && <HeaderCartButton onClick={props.onShowCart} />}
       </header>
     </Fragment>
   );
