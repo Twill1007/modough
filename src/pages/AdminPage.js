426e08bb512c38e2139d82cd3e460a3905485c21
Twill1007 <85330522+Twill1007@users.noreply.github.com>
2023-09-08 22:30:52 -0700

    CSS&AdminPage

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,11 +1,33 @@
-import React from "react";
-import TempNavBar from "../components/Layout/TempNavBar";
+import React, { Fragment, useState } from "react";
 
 function AdminPage() {
+  const [newOrders, setNewOrders] = useState(false);
+  const [filledOrders, setFilledOrders] = useState(false);
+  const [shippedOrders, setShippedOrders] = useState(false);
+
+  const newOrderHandler = () => {
+    setNewOrders(true);
+  };
+
+  const filledOrderHandler = () => {
+    setFilledOrders(true);
+  };
+
+  const shippedOrderHandler = () => {
+    setShippedOrders(true);
+  };
+
   return (
-    <TempNavBar>
-      <h1>Admin Page</h1>;
-    </TempNavBar>
+    <Fragment>
+      <div>
+        <button onClick={newOrderHandler}>New Orders</button>
+        <button onClick={filledOrderHandler}>Filled Orders</button>
+        <button onClick={shippedOrderHandler}>Shipped Orders</button>
+        <div>{newOrders && <h1>New Order</h1>}</div>
+        <div>{filledOrders && <h1>Filled Order</h1>}</div>
+        <div>{shippedOrders && <h1>Completed Order</h1>}</div>
+      </div>
+    </Fragment>
   );
 }
 
